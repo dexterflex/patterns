4d98f039a85fc9a6e62835d9566130b5d850d044
VIVEK KUMAR <vivekkumar@VIVEKs-MacBook-Air.local>
2024-05-28 03:12:17 +0530

    diamond.java added

--- diamond.java
+++ diamond.java
@@ -6,5 +6,27 @@ public class diamond {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
+
+        // loop
+        for (int i = 1; i <= 2 * num - 1; i++) {
+            if (i <= num) {
+                for (int j = num; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int j = num; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = num - (i - num); j > 0; j--) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
