2d26888bca0740cbe3096299e164e0dd1fd3b77f
VIVEK KUMAR <vivekkumar@VIVEKs-MacBook-Air.local>
2024-05-28 00:45:19 +0530

    rhombus.java added

---
+++ diamond.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class diamond {
+    public static void main(String[] args) {
+        // inputs
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+    }
+}
